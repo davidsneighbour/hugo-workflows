035f0c27c466f3de021e1ad4883e5563e4f18f48
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-01-05 23:39:36 +0700

    chore: add release scripts

---
+++ bin/release-hook-postrelease.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+REQUIRED_TOOLS=(
+  git
+)
+
+for TOOL in "${REQUIRED_TOOLS[@]}"; do
+  if ! command -v "${TOOL}" >/dev/null; then
+    echo "${TOOL} is required... "
+    exit 1
+  fi
+done
+
+git push --follow-tags origin main
+git push --tags
